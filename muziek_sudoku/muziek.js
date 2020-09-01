ebe60bd13d1679668f06c525207c28d5e2effcff
Davpouck <50929168+Davpouck@users.noreply.github.com>
2020-09-01 21:24:23 +0200

    Add files via upload

--- muziek_sudoku/muziek.js
+++ muziek_sudoku/muziek.js
@@ -1,7 +1,10 @@
 "use strict"
 
 let playing_music = document.createElement('audio');
+let playing_target = null;
 playing_music.src = null;
+playing_music.controls = true;
+playing_music.style.position = "absolute";
 document.body.append(playing_music);
 
 document.addEventListener('click', function(event) {
@@ -12,9 +15,14 @@ document.addEventListener('click', function(event) {
 function startMusic(target) {
     if (playing_music.src) {
         playing_music.pause();
+        if (playing_target) {
+            playing_target.style.color = null;
+        }
     }
 
-    playing_music.src = "audio/t" + target.id[0] + "/" + target.id[1] + ".mp3";
+    playing_target = target;
+    playing_music.src = "assets/audio/t" + target.id[0] + "/" + target.id[1] + ".mp3";
+    target.style.color = "#46aebc";
     playing_music.play();
 }
 
